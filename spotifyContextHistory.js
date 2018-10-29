d8e58e23f063c2d0beb4a563bfe0fc0ff2eb6842
António Ramadas <antonio_ramadas@hotmail.com>
2018-10-29 19:31:12 +0000

    Add TODOs

---
+++ spotifyContextHistory.js
@@ -0,0 +1,12 @@
+
+// TODO constructor with access and refresh tokens
+
+// TODO: self.healing process if access token is expired
+
+// TODO: get context
+
+// TODO: merge contexts
+
+// TODO: context states
+
+// TODO: play from context
