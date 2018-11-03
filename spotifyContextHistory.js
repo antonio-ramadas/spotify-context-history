eb93cf22cd2e7770aa2f2db21b236535151acbdc
António Ramadas <antonio_ramadas@hotmail.com>
2018-11-03 19:07:15 +0000

    Add `play` method

--- spotifyContextHistory.js
+++ spotifyContextHistory.js
@@ -12,7 +12,14 @@ class SpotifyContextHistory {
         // Currently, Spotify only retrieves at most 50 tracks per request
         this.limitOfTracksPerRequest = 50;
 
+        // `contextHistory` is a map of string to string
+        // the keys are the uri of the context (album or playlist)
+        // the values are the tracks of the last track played in the given context
         this.contextHistory = [];
+    }
+
+    getContextHistory() {
+        return this.contextHistory;
     }
 
     setContextHistory(newContext) {
@@ -51,7 +58,14 @@ class SpotifyContextHistory {
         return this.spotifyWebApi.getTrack(getId(uri)).then(res => res.body);
     }
 
-    // TODO: play from context
+    // If the context has not been found, it tries to play from the beginning
+    play(context) {
+        return this.spotifyWebApi.play({
+            context_uri: context,
+            offset: { uri: this.contextHistory[context] },
+        })
+            .catch(() => this.spotifyWebApi.play({ context_uri: context }));
+    }
 }
 
 module.exports = SpotifyContextHistory;
